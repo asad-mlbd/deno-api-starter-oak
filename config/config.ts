60ca0e080493ead5a554cc7ba9b06238dd5a7217
S M Asad Rahman <asad.rahman@monstar-lab.com.bd>
2020-05-30 02:42:11 +0600

    configuration setup, db resource

---
+++ config/config.ts
@@ -0,0 +1,2 @@
+import { config as loadConfig } from "https://deno.land/x/dotenv/mod.ts";
+export const config = loadConfig();
---
+++ db/db.ts
@@ -0,0 +1,14 @@
+import { Client } from "https://deno.land/x/mysql/mod.ts";
+import { config } from "./../config/config.ts";
+
+const port = config.DB_PORT ? parseInt(config.DB_PORT || "") : undefined;
+
+const db = await new Client().connect({
+  port,
+  hostname: config.DB_HOST,
+  username: config.DB_USER,
+  db: config.DB_NAME,
+  password: config.DB_PASS,
+});
+
+export { db };
