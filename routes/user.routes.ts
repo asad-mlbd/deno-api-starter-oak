2b1f8b5416505f0021696e9621b41bc3292d84f3
S M Asad Rahman <asad.rahman@monstar-lab.com.bd>
2020-05-31 00:00:05 +0600

    query helper added in user routes

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,17 +1,18 @@
+import { Context, helpers } from "https://deno.land/x/oak/mod.ts";
 import * as userRepo from "./../repositories/user.repository.ts";
 
-const getUsers = async (ctx: any) => {
+const getUsers = async (ctx: Context) => {
   const users = await userRepo.getUsers();
   ctx.response.body = users;
 };
 
-const getUserById = async (ctx: any) => {
-  const { id } = ctx.params;
+const getUserById = async (ctx: Context) => {
+  const { id } = helpers.getQuery(ctx, { mergeParams: true });
   const user = await userRepo.getUserById(id);
   ctx.response.body = user;
 };
 
-const createUser = async (ctx: any) => {
+const createUser = async (ctx: Context) => {
   const request = ctx.request;
   const userData = (await request.body()).value;
   const user = await userRepo.createUser(userData);
