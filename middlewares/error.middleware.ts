e060738be7790507871dbec74febab6b1dbd1840
S M Asad Rahman <asad.rahman@monstar-lab.com.bd>
2020-06-08 14:46:41 +0600

    error middleware return json error message with status

--- middlewares/error.middleware.ts
+++ middlewares/error.middleware.ts
@@ -9,15 +9,22 @@ const errorMiddleware = async (ctx: Context, next: () => Promise<void>) => {
   try {
     await next();
   } catch (err) {
-    if (!isHttpError(err)) {
-      /** internal error message should not be shown other than dev env */
-      const message = config.ENV === "dev" || config.ENV === "development"
-        ? err.message
-        : "Internal Server Error";
+    let message = err.message;
+    const status = err.status || err.statusCode || Status.InternalServerError;
 
-      ctx.response.body = { message };
-      ctx.response.status = Status.InternalServerError;
+    /**
+     * considering all unhandled errors as internal server error,
+     * do not want to share internal server errors to 
+     * end user in non "development" mode
+     */
+    if (!isHttpError(err)) {
+      message = config.ENV === "dev" || config.ENV === "development"
+        ? message
+        : "Internal Server Error";
     }
+
+    ctx.response.status = status;
+    ctx.response.body = { status, message };
   }
 };
 
