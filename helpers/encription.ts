f770edea905efce3254b2a9ebb7d8bd0907ddf55
S M Asad Rahman <asad.rahman@monstar-lab.com.bd>
2020-06-10 21:50:00 +0600

    scrypt password encription helper

---
+++ helpers/encription.ts
@@ -0,0 +1,16 @@
+import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.1/mod.ts";
+/**
+ * encript given string 
+ */
+const encript = async (password: string) => {
+  return await bcrypt.hash(password);
+};
+
+/**
+ * compare given password and hash
+ */
+const compare = async (password: string, hash: string) => {
+  return await bcrypt.compare(password, hash);
+};
+
+export { encript, compare };
