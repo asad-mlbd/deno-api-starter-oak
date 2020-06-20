012e5e58e06ec3389555f563e59099c08c95be9e
Asad Rahman <asad.rahman@monstar-lab.com.bd>
2020-06-21 02:25:04 +0600

    cors enabled for all

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import { Application } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 import * as middlewares from "./middlewares/middlewares.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import { router } from "./routes/routes.ts";
 import { Context } from "./types.ts";
 
 const port = 8000;
 const app = new Application<Context>();
 
+app.use(oakCors());
 app.use(middlewares.errorMiddleware);
 app.use(middlewares.loggerMiddleware);
 app.use(middlewares.timingMiddleware);
