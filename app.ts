fa740d95bbbab1788db9f1a46a776615e100bfd3
Asad Rahman <asad.rahman@monstar-lab.com.bd>
2020-06-28 20:48:34 +0600

    refactor JWT auth middleware

--- app.ts
+++ app.ts
@@ -3,15 +3,19 @@ import * as middlewares from "./middlewares/middlewares.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import { router } from "./routes/routes.ts";
 import { Context } from "./types.ts";
+import { config } from "./config/config.ts";
 
 const port = 8000;
 const app = new Application<Context>();
+
 
 app.use(oakCors());
 app.use(middlewares.errorMiddleware);
 app.use(middlewares.loggerMiddleware);
 app.use(middlewares.timingMiddleware);
-app.use(middlewares.JWTAuthMiddleware);
+
+const { JWT_TOKEN_SECRET } = config;
+app.use(middlewares.JWTAuthMiddleware(JWT_TOKEN_SECRET));
 
 app.use(router.routes());
 app.use(router.allowedMethods());
